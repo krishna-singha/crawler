8609b1a69a71fed59ec457760531526c4a6599d9
Krishna Singha <krishnasingha.dev@gmail.com>
2025-04-03 03:59:57 +0530

    minor change

--- src/FavIconExtractor.py
+++ src/FavIconExtractor.py
@@ -21,14 +21,6 @@ class FavIconExtractor:
         except httpx.RequestError as e:
             print(f"Error fetching page: {e}")
             return BeautifulSoup("", "html.parser")
-        
-    def is_valid_url(self) -> bool:
-        """Check if the URL is valid and reachable."""
-        try:
-            response = httpx.head(self.url, timeout=10)
-            return response.status_code == 200
-        except httpx.RequestError:
-            return False
 
     def get_favicon(self) -> str | None:
         """Extracts the favicon URL, handling both absolute and relative URLs."""
@@ -39,15 +31,23 @@ class FavIconExtractor:
         favicon_link = self.soup.find("link", rel=lambda rel: rel and "icon" in rel.lower())
         if favicon_link and "href" in favicon_link.attrs:
             url = urljoin(self.url, favicon_link["href"])
-            if(self.is_valid_url()):
+            if(is_valid_url(url)):
                 return url
             else:
                 return None
 
         # Fallback: Common default location for favicons
         return None
+    
+def is_valid_url(url: str) -> bool:
+    """Check if the URL is valid and reachable."""
+    try:
+        response = httpx.head(url, timeout=10)
+        return response.status_code == 200
+    except httpx.RequestError:
+        return False
 
 # # Example Usage:
-# url = "http://cse.iitkgp.ac.in"
+# url = "https://hmc.iitkgp.ac.in/"
 # fav_extractor = FavIconExtractor(url)
 # print(fav_extractor.get_favicon())
